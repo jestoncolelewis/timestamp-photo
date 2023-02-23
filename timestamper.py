62596d7f6a3e71ad432b605d4f18191dbe6f0054
Jeston Lewis <jeston@jestonlewiscreative.com>
2023-02-23 16:16:13 -0500

    complete

---
+++ timestamper.py
@@ -0,0 +1,34 @@
+from PIL import Image, ExifTags
+import cv2 as cv
+import os
+import glob
+
+path = os.getcwd()
+
+while True:
+        if os.path.exists("./input") is True:
+            break
+        else:
+            os.mkdir("./input")
+
+while True:
+        if os.path.exists("./output") is True:
+            break
+        else:
+            os.mkdir("./output")
+
+images = glob.glob("input/*")
+
+
+for image in images:
+    exif_img = Image.open(image)
+    exif_data = { ExifTags.TAGS[k]: v for k, v in exif_img._getexif().items() if k in ExifTags.TAGS } 
+    datetime = exif_data['DateTimeOriginal']
+    print(datetime)
+
+    img = cv.imread(image)
+    img_name = image[len("input/") :]
+    img_text = cv.putText(img, datetime, (10, 100), cv.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 5)
+
+    new_name = f"ts_{img_name}"
+    cv.imwrite(os.path.join(f"{path}/output", new_name), img_text)
