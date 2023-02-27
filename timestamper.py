6f881b8ac9ce7a5b2e458916a92cca7227a8ea34
Jeston Lewis <jeston@jestonlewiscreative.com>
2023-02-27 14:39:38 -0600

    added text input

--- timestamper.py
+++ timestamper.py
@@ -16,8 +16,13 @@ def openfile():
     images = fd.askopenfilenames()
 images = ()
 open_button = tk.Button(window, text='OPEN', command=openfile)
-open_button.pack()
 
+street = tk.Entry(window)
+city = tk.Entry(window)
+state = tk.Entry(window)
+zip = tk.Entry(window)
+
+# process function
 def process():
     path = fd.askdirectory()
     for image in images:
@@ -25,18 +30,28 @@ def process():
         exif_data = { ExifTags.TAGS[k]: v for k, v in exif_img.getexif().items() if k in ExifTags.TAGS }
         datetime = exif_data['DateTime']
         
+        text = [datetime, street.get(), city.get(), state.get(), zip.get()]
+        y = 100
 
         img = cv.imread(image)
         img_name = image[:]
         name_l = image.rfind('/')
         img_name = image[name_l+1:]
-        img_text = cv.putText(img, datetime, (10, 100), cv.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 5)
+        for i in text:
+            img_text = cv.putText(img, i, (10, y), cv.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 5)
+            y += 105
 
         new_name = f"ts_{img_name}"
         cv.imwrite(os.path.join(f"{path}/", new_name), img_text)
 
 # process action button
 process_button = tk.Button(text='PROCESS', command=process)
+
+street.pack()
+city.pack()
+state.pack()
+zip.pack()
+open_button.pack()
 process_button.pack()
 
 window.mainloop()
